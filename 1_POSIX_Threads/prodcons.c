048e32fde6bf0c3cf2858c9118c19f11d465cee8
Jia Zhang <facade1801@163.com>
2020-10-28 21:52:36 -0500

    Revert "doesn't work but temporily store it"
    This reverts commit 2995e0ed846df51ee95f774b546cc7e9a3f9e467.

--- 1_POSIX_Threads/prodcons.c
+++ 1_POSIX_Threads/prodcons.c
@@ -10,33 +10,45 @@ int Buffer[5];
 int in = 0;
 int out = 0;
 int count = 0;
+int currentColor = 0; // first red.
 
 pthread_mutex_t  lock;
 pthread_cond_t  SpaceAvailable, ItemAvailable;
+pthread_cond_t nextRed, nextBlue;
 
-
+// color: red(0), blue(1), white(2)
 void * producer (void *arg)
 {
+	int color;
+	color = (int*) arg;
+	printf("color is %d\n", color);
 	int i;
 	for ( i = 0; i< 20; i++) {
 		// Enter critical section
 		pthread_mutex_lock ( &lock);
 		// Make sure that buffer is NOT full
-		while ( count == N )
+		while ( count == N || currentColor != color) {
 			// Sleep using a cond variable
 			while (  pthread_cond_wait( &SpaceAvailable, &lock) != 0 )
 				;
-		// printf( "Producer %d \n", i);
+		}
+		// if (color == 0) { while( pthread_cond_wait( &nextRed, &lock ) != 0) ; }
+		// else { while( pthread_cond_wait( &nextBlue, &lock ) != 0) ; }
 		
-		// count must be less than N
+		// count must be less than N。 TODO: Why is this not implemented??
 		// Put item in the buffer using "in"
 		Buffer[in] = i;
 		in = (in + 1) % N;
 
 		// Increment the count of items in the buffer
 		count++;
-		printf("Deposited item = %d\n", i);
+		printf("Deposited item (color:%d) = %d\n", color, i);
 		
+		//sanity check
+		if (color != currentColor) printf("the color is not corresponding");
+		if (color == 0) currentColor = 1;
+		if (color == 1) currentColor = 0;
+
 		pthread_mutex_unlock ( &lock);
 		// Wakeup consumer, if waiting
 		pthread_cond_signal( &ItemAvailable );
@@ -93,23 +105,32 @@ int main( int argc, char* argv[] )
 	printf("Start running time (s + ms):  %ld\n", currentTime.tv_sec*1000000 + currentTime.tv_usec);
 	printf("Start running time (seconds): %ld\n", currentTime.tv_sec);
 	printf("Start running time (microseconds):      %ld\n", currentTime.tv_usec);
-
+	
 	// thread variables
-	pthread_t prod, cons;
+	pthread_t prod1, prod2, cons;
 	// attribute object
 	pthread_attr_t attr;
 	int n;
-	
+
 	pthread_mutex_init( &lock, NULL);
 	pthread_cond_init( &SpaceAvailable, NULL);
 	pthread_cond_init( &ItemAvailable, NULL);
-	
-	// Create producer thread
-	if ( n = pthread_create(&prod, NULL, producer ,NULL)) {
+	pthread_cond_init( &nextBlue, NULL);
+	pthread_cond_init( &nextRed, NULL);
+
+	int red = 0;
+	int blue = 1;
+	printf("flag 1\n");
+	// Create 3 producer threads
+	if ( n = pthread_create(&prod1, NULL, producer, (void*) red)) {
 		fprintf(stderr,"pthread_create :%s\n",strerror(n));
 		exit(1);
 	}
-	
+	if ( n = pthread_create(&prod2, NULL, producer, (void*) blue)) {
+		fprintf(stderr,"pthread_create :%s\n",strerror(n));
+		exit(1);
+	}
+
 	// Create consumer thread
 	if (  n = pthread_create(&cons, NULL, consumer, NULL) ) {
 		fprintf(stderr,"pthread_create :%s\n",strerror(n));
